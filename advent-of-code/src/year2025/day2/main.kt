f6425243251f3ebfa5ff0edafc86ec3848db33c0
Henry Walsh <50926470+henrywalsh@users.noreply.github.com>
2025-12-02 12:24:48 +0100

    day 2 add time blocks

--- advent-of-code/src/year2025/day2/main.kt
+++ advent-of-code/src/year2025/day2/main.kt
@@ -1,11 +1,22 @@
 package year2025.day2
 
+import kotlin.time.measureTime
+
 fun main() {
     val filePath = "src/year2025/day2/puzzleInput.txt"
-
     val invalidProductFinderPartOne = InvalidProductFinderPartOne()
     val invalidProductFinderPartTwo = InvalidProductFinderPartTwo()
 
-    println(invalidProductFinderPartOne.findSumOfInvalidIds(filePath))
-    println(invalidProductFinderPartTwo.findSumOfInvalidIds(filePath))
+
+    var answerPartOne: Long = 0
+    val timePartOne = measureTime {
+        answerPartOne = invalidProductFinderPartOne.findSumOfInvalidIds(filePath)
+    }
+    println(String.format("Part One: answer %d, time %s", answerPartOne, timePartOne.toString()))
+
+    var answerPartTwo: Long = 0
+    val timePartTwo = measureTime {
+        answerPartTwo = invalidProductFinderPartTwo.findSumOfInvalidIds(filePath)
+    }
+    println(String.format("Part Two: answer %d, time %s", answerPartTwo, timePartTwo.toString()))
 }
